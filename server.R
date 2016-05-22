e9f931813342970f41075768a871773479d9de57
Paul Nulty <paul.nulty@gmail.com>
2016-05-23 00:32:52 +0100

    working prototypes: file upload, dfm, kwic

--- server.R
+++ server.R
@@ -1,23 +1,86 @@
-
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
 # http://shiny.rstudio.com
 #
-
 library(shiny)
+library(quanteda)
+library(dplyr)
+
+getCorpus <- function(path){
+  if(is.null(path)){
+    return(NULL)
+  }
+  print(path$datapath)
+  tmpPath <- tempdir
+  fs <- path$datapath
+  for (i in 1:length(fs)){
+    textsvec[i] <- paste(readLines(fs[i]), collapse = " ")
+  }
+  names(textsvec) <- basename(path$name)
+  qc <- corpus(textsvec)
+  return(qc)
+}
+
+
+getCorpusFromZip <- function(path, fixedZip = FALSE){
+  if(is.null(path)){
+    return(NULL)
+  }
+  if(fixedZip){
+    # need an altered version of textfiles.R to make this work 
+    # (around line 298)
+    qc <- (textfile(path))
+  }
+  else{
+    td <- tempdir()
+    fs <- unzip(path$datapath, exdir = td)
+    textsvec = c()
+    for (i in 1:length(fs)){
+      textsvec[i] <- paste(readLines(fs[i]), collapse = " ")
+    }
+    names(textsvec) <- basename(fs)
+    qc <- corpus(textsvec)
+  }
+  return(qc)
+}
+
 
 shinyServer(function(input, output) {
+  myCorpus <- reactive({
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    if(input$uploadTypeRadio == 1){
+      mc <- getCorpusFromZip(inFile)
+    }
+    else{
+      mc <- getCorpus(inFile)
+    }
+    return(mc)
   })
+  output$fileTable <- renderDataTable({
+    curCorpus <- myCorpus()
+    summary(curCorpus)
+  }, options = list(searching = FALSE))
+  output$dfmTable <-  renderDataTable({
+    curCorpus <- myCorpus()
+    weightType = 'relFreq'
+    curDf <- dfm(curCorpus) %>% trim(as.numeric(input$minFreq), as.numeric(input$minDoc))
+    if(input$weightingRadio != "count"){
+      curDf <- weight(curDf, input$weightingRadio)
+    }
+    showN <- as.numeric(input$showN)
+    curDf <- as.data.frame(curDf)
+    toRender <- cbind(filenames = row.names(curDf[,1:showN]), curDf[,1:showN])
+  }, options = list(searching = FALSE))
+  
+  output$kwicTable <- renderDataTable({
+    curCorpus <- myCorpus()
+    kwic(curCorpus, input$keyword, window = input$contextSize,
+         valuetype = input$kwicValueType,
+         case_insensitive = !(input$caseSensitive))
+  }, options = list(searching = FALSE))
 
 })
--- ui.R
+++ ui.R
@@ -6,25 +6,88 @@
 #
 
 library(shiny)
+####################
+# Tab One: File Upload
+###################
+uploadPanel <- tabPanel("File Upload",
+                        sidebarLayout(
+                          sidebarPanel(
+                            fileInput('file1', 'Choose file to upload',
+                                      accept = c(
+                                     'text/csv',
+                                     'text/comma-separated-values',
+                                     'text/tab-separated-values',
+                                     'text/plain',
+                                     '.csv',
+                                     '.tsv'
+                         ), multiple=TRUE),
+               radioButtons("uploadTypeRadio", label = "Upload type",
+                            choices = list("zip file" = 1, ".txt files" = 2), 
+                            selected = 1)
+             ),
+             # Show a plot of the generated distribution
+             mainPanel(
+               dataTableOutput("fileTable")
+             )
+           )
+)
+
+###################
+# Tab Two: Dfm
+###################
+dfmPanel <- tabPanel("DFM",
+         sidebarLayout(
+           sidebarPanel(
+             "Trim features by word and doc frequency",
+             textInput("minFreq", label = "Min. frequency", value = 1),
+             textInput("minDoc", label = "Min. document frequency", value = 1),
+             hr(),
+             tmpLabel <- "Number of features to display (displaying all can be slow).",
+             textInput("showN", label = tmpLabel, value = 100),
+             hr(),
+             "Feature weighting",
+             radioButtons("weightingRadio", label = "Type of feature weighting",
+                          choices = list("Count" = "count", "tf-idf" = "tfidf", "Relative frequency" = "relFreq"), 
+                          selected = "count")
+             
+           ),
+           # Show a plot of the generated distribution
+           mainPanel(
+             dataTableOutput("dfmTable")
+           )
+         ))
+
+###################
+# Tab Three: Kwic
+###################
+kwicPanel <- tabPanel("Keyword-in-context",
+         sidebarLayout(
+           sidebarPanel(
+             p("Keyword in context search"),
+             textInput("keyword", label = "Search string"),
+             hr(),
+             sliderInput("contextSize", "Context size", min=3, max=50, value=5, step=1),
+             radioButtons("kwicValueType", label = "Type of search string",
+                          choices = list("glob" = "glob", "regex" = "regex", "fixed" = "fixed"), 
+                          selected = "glob"),
+             checkboxInput("caseSensitive", "Case Sensitive")
+           ),
+           # Show a plot of the generated distribution
+           mainPanel(
+             dataTableOutput("kwicTable")
+           )
+         )
+)
 
 shinyUI(fluidPage(
-
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Quanteda"),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+  tabsetPanel(
+    uploadPanel,
+    dfmPanel,
+    kwicPanel
     )
   )
-))
+)
